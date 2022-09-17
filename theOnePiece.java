9890fb4333e95a79b1bfe880ca61d1408162076d
revilo vader <oliver.cushman@mrjaffesclass.com>
2022-09-17 12:12:20 -0700

    can we get much higher

---
+++ theOnePiece.java
@@ -0,0 +1,10 @@
+public class theOnePiece
+{
+    public static void main(String[] args)
+    {
+        int i = 0;
+        while (i == 0) {
+            System.out.println("THE ONE PIECE, THE ONE PIECE IS REAL!!!!");
+        }
+    }
+}
